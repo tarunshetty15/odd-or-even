d8d8ce165841bf4181f3b70bbd7a37e53d25921c
BCA 39 <BCA 39@NIPE-LAB3-029>
2025-08-07 12:35:37 +0530

    Add project files.

---
+++ odd or even/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace odd_or_even
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number");
+            if (! int.TryParse(Console.ReadLine(), out int num))
+            {
+                Console.WriteLine("Invalid number");
+                return;
+            }
+            Console.WriteLine($"{num} is {(num % 2 == 0 ? "Even" : "Odd")}");
+        }
+    }
+}
